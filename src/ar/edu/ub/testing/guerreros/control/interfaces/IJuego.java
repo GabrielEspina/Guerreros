707342ccf24b84ecbce0056588cd238e027e2473
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2018-12-12 21:29:08 -0300

    Se documenta la interfaz IJuego

--- src/ar/edu/ub/testing/guerreros/control/interfaces/IJuego.java
+++ src/ar/edu/ub/testing/guerreros/control/interfaces/IJuego.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 
 public interface IJuego {
 	
+	/*--Variables estaticas para parametrizar el menu principal--*/
+	public static final int MP_MIN = 1;
+	public static final int MP_MAX = 4;
 	public static final int MENU_PRINCIPAL = 1;
 	public static final int MENU_SELECCION_JUEGO = 1;
 	public static final int MENU_RECORDS = 2;
 	public static final int MENU_TUTORIAL = 3;
 	public static final int SALIR = 4;
 	
+	/*--Variables estaticas para parametrizar el menu records--*/
+	public static final int MR_MIN = 1;
+	public static final int MR_MAX = 3;
 	public static final int MENU_RECORDS_SINGLEPLAYER = 1;
 	public static final int MENU_RECORDS_MULTIPLAYER = 2;
 	public static final int MENU_RECORDS_VOLVER = 3;
