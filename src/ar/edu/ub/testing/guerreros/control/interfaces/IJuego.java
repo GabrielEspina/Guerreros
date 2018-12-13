206f55ba3d704765a004a2ca4b2e01502f56171f
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2018-12-12 22:17:37 -0300

    Se crean las variables estaticas para tutorial

--- src/ar/edu/ub/testing/guerreros/control/interfaces/IJuego.java
+++ src/ar/edu/ub/testing/guerreros/control/interfaces/IJuego.java
@@ -20,6 +20,18 @@ public interface IJuego {
 	public static final int MENU_RECORDS_MULTIPLAYER = 2;
 	public static final int MENU_RECORDS_VOLVER = 3;
 	
+	/*--Variables estaticas para parametrizar el menu seleccion juego--*/
+	public static final int MS_MIN = 1;
+	public static final int MS_MAX = 3;
+	public static final int MENU_SELECCION = 2;
+	public static final int MENU_SELECCION_SINGLEPLAYER = 1;
+	public static final int MENU_SELECCION_MULTIPLAYER = 2;
+	public static final int MENU_SELECCION_VOLVER = 3;
+	
+	//Parametrizacion del menu tutorial
+	public static final int MT_MIN = 1;
+	public static final int MT_MAX = 1;
+	
 	
 	public void ejecutar() throws ClassNotFoundException, IOException;
 
